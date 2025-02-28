d6065e11cafe3b3b2ded40b39b8465ae4760f982
TheJegede <112409241+TheJegede@users.noreply.github.com>
2025-02-27 21:53:58 -0500

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_khGoSJHaK', 
+        userPoolClientId: '28apg07kocpo2i8kb8hr397pmq', 
+        region: 'us-east-2'
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
