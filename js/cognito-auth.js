d4c781bbf7dd47bf5a768a52cae1807a8bbe49bb
TheJegede <112409241+TheJegede@users.noreply.github.com>
2025-02-27 23:57:37 -0500

    Create cognito-auth.js

---
+++ js/cognito-auth.js
@@ -0,0 +1,67 @@
+/*global StudentData _config AmazonCognitoIdentity AWSCognito*/
+
+var StudentData = window.StudentData || {};
+
+(function scopeWrapper($) {
+    var signinUrl = '/signin.html';
+
+    var poolData = {
+        UserPoolId: _config.cognito.userPoolId,
+        ClientId: _config.cognito.userPoolClientId
+    };
+
+    var userPool;
+
+    if (!(_config.cognito.userPoolId &&
+          _config.cognito.userPoolClientId &&
+          _config.cognito.region)) {
+        $('#noCognitoMessage').show();
+        return;
+    }
+
+    userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+    if (typeof AWSCognito !== 'undefined') {
+        AWSCognito.config.region = _config.cognito.region;
+    }
+
+    StudentData.signOut = function signOut() {
+        userPool.getCurrentUser().signOut();
+    };
+
+    StudentData.authToken = new Promise(function fetchCurrentAuthToken(resolve, reject) {
+        var cognitoUser = userPool.getCurrentUser();
+
+        if (cognitoUser) {
+            cognitoUser.getSession(function sessionCallback(err, session) {
+                if (err) {
+                    reject(err);
+                } else if (!session.isValid()) {
+                    resolve(null);
+                } else {
+                    resolve(session.getIdToken().getJwtToken());
+                }
+            });
+        } else {
+            resolve(null);
+        }
+    });
+
+
+    /*
+     * Cognito User Pool functions
+     */
+
+    function register(email, password, onSuccess, onFailure) {
+        var dataEmail = {
+            Name: 'email',
+            Value: email
+        };
+        var attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute(dataEmail);
+
+        userPool.signUp(toUsername(email), password, [attributeEmail], null,
+            function signUpCallback(err, result) {
+                if (!err) {
+                    onSuccess(result);
+                } else {
+}(jQuery));
